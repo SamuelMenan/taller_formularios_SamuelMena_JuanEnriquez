9c09aab866fa661e4414c1e7d1d775663984b5e3
Juan Enriquez <juan.enriquezc@campusucc.edu.co>
2026-04-16 11:30:25 -0500

    feat: add asistencia model

--- taller_formularios_SamuelMena_JuanEnriquez/asistencia/models.py
+++ taller_formularios_SamuelMena_JuanEnriquez/asistencia/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Asistencia(models.Model):
+    nombre_completo = models.CharField(max_length=150)
+    documento = models.CharField(max_length=50)
+    correo = models.EmailField()
+    fecha = models.DateField()
+    hora_ingreso = models.TimeField()
+    hora_salida = models.TimeField()
+    presente = models.BooleanField()
+    observaciones = models.TextField(blank=True, null=True)
